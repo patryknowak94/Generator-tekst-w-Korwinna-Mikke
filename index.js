ae6311d442f515c453ad4108836044ba5af30bb6
patryk.nowak <132762302+patros10a@users.noreply.github.com>
2023-06-10 21:47:42 +0200

    Powrót do poprzedniej wersji

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Druga = ['przedstawiciele czerwonej hołoty', 'ci wszyscy (tfu!) geje', 'f
 const Trzecia = ['zupełnie bezkarnie', 'całkowicie bezczelnie', 'o poglądach na lewo od komunizmu', 'celowo i świadomie', 'z premedytacją', 'od czasów Okrągłego Stołu', 'w ramach postępu']
 const Czwarta = ['nawołują do podniesienia podatków', 'próbują wyrzucić kierowców z miast', 'próbują skłócić Polskę z Rosją ', 'głoszą brednie o globalnym ociepleniu', 'zakazują posiadania broni', 'nie dopuszczają prawicy do władzy', 'uczą dzieci homoseksualizmu']
 const Piąta = ['bo dzięki temu mogą kraść', 'bo dostają za to pieniądze', 'bo tak się uczy w państwowej szkole', 'bo bez tego (tfu!) demokracja nie moze istnieć', 'bo głupich jest więcej nit mądrych', 'bo chcą tworzyć raj na ziemi', 'bo chcą niszczyć cywilizację białego człowieka']
-const Szósta = ['przez kolejne kadencje', 'o czym się nie mówi', 'i właśnie dlatego Europa umiera', 'ale przyjdą muzułmanie i zrobią porządek', 'tak samo zresztą Jak za Hitlera', 'proszę zobaczyć, co się dzieje na Zachodzie, jeśli mi państwo nie wierzą', 'co lat temu sto nikomu nie przyszłoby nawet do głowy ']
+const Szósta = ['przez kolejne kadencje', 'o czym się nie mówi', 'i właśnie dlatego Europa umiera', 'ale przyjdą muzułmanie i zrobią porządek', 'tak samo zresztą Jak za Hitlera', 'proszę zobaczyć, co się dzieje na Zachodzie, jeśli mi państwo nie wierzą', 'co lat temu sto nikomu nie przyszłoby nawet do głowy']
 
 const zdanie = () => {
     const num1 = () => {
