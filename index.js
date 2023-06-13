e055ce69ea5bcd86aa0b10341bf30294e8d3151f
patryk.nowak <132762302+patros10a@users.noreply.github.com>
2023-06-13 18:49:03 +0200

    Fix to scale of the random number - now it's base on arrow length no "7"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Pierwsza = ['Ja chcę powiedzieć jedną rzecz:', 'Trzeba powiedzieć jasno:', 'Jak powiedział wybitny krakowianin Stanisław Lem.', 'Proszę mnie dobrze zrozumieć:', 'Ja chciałem państwu przypomnieć ze', 'Niech państwo nie mają złudzeń:', 'Powiedzmy to wyraźnie:']
+const Pierwsza = ['Ja chcę powiedzieć jedną rzecz:', 'Trzeba powiedzieć jasno:', 'Jak powiedział wybitny krakowianin Stanisław Lem.', 'Proszę mnie dobrze zrozumieć:', 'Ja chciałem państwu przypomnieć że', 'Niech państwo nie mają złudzeń:', 'Powiedzmy to wyraźnie:']
 const Druga = ['przedstawiciele czerwonej hołoty', 'ci wszyscy (tfu!) geje', 'funkcjonańusze reżymowej telewizji', 'tak zwani ekolodzy', 'ci wszyscy (tfu!) demokraci', 'agenci bezpieki', 'feminazistki']
 const Trzecia = ['zupełnie bezkarnie', 'całkowicie bezczelnie', 'o poglądach na lewo od komunizmu', 'celowo i świadomie', 'z premedytacją', 'od czasów Okrągłego Stołu', 'w ramach postępu']
 const Czwarta = ['nawołują do podniesienia podatków', 'próbują wyrzucić kierowców z miast', 'próbują skłócić Polskę z Rosją ', 'głoszą brednie o globalnym ociepleniu', 'zakazują posiadania broni', 'nie dopuszczają prawicy do władzy', 'uczą dzieci homoseksualizmu']
@@ -7,31 +7,32 @@ const Szósta = ['przez kolejne kadencje', 'o czym się nie mówi', 'i właśnie
 
 const zdanie = () => {
     const num1 = () => {
-        return Math.floor(Math.random()*7)
+        return Math.floor(Math.random()*Pierwsza.length)
     }
 
     const num2 = () => {
-        return Math.floor(Math.random()*7)
+        return Math.floor(Math.random()*Druga.length)
     }
 
     const num3 = () => {
-        return Math.floor(Math.random()*7)
+        return Math.floor(Math.random()*Trzecia.length)
     }
 
     const num4 = () => {
-        return Math.floor(Math.random()*7)
+        return Math.floor(Math.random()*Czwarta.length)
     }
 
     const num5 = () => {
-        return Math.floor(Math.random()*7)
+        return Math.floor(Math.random()*Piąta.length)
     }
 
     const num6 = () => {
-        return Math.floor(Math.random()*7)
+        return Math.floor(Math.random()*Szósta.length)
     }
 
     console.log(`${Pierwsza[num1()]} ${Druga[num2()]} ${Trzecia[num3()]} ${Czwarta[num4()]} ${Piąta[num5()]} ${Szósta[num6()]}!`)
     
     return ''
 }
-console.log(zdanie())
+
+zdanie()
