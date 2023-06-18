46f1cf3dde1d993de154fd212238d9a830becd97
patryk.nowak <132762302+patros10a@users.noreply.github.com>
2023-06-18 14:43:43 +0200

    HTML i CSS ADD

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Pierwsza = ['Ja chcę powiedzieć jedną rzecz:', 'Trzeba powiedzieć jasno:', 'Jak powiedział wybitny krakowianin Stanisław Lem.', 'Proszę mnie dobrze zrozumieć:', 'Ja chciałem państwu przypomnieć że', 'Niech państwo nie mają złudzeń:', 'Powiedzmy to wyraźnie:']
-const Druga = ['przedstawiciele czerwonej hołoty', 'ci wszyscy (tfu!) geje', 'funkcjonańusze reżymowej telewizji', 'tak zwani ekolodzy', 'ci wszyscy (tfu!) demokraci', 'agenci bezpieki', 'feminazistki']
+const Druga = ['przedstawiciele czerwonej hołoty', 'ci wszyscy (tfu!) geje', 'funkcjonarusze reżymowej telewizji', 'tak zwani ekolodzy', 'ci wszyscy (tfu!) demokraci', 'agenci bezpieki', 'feminazistki']
 const Trzecia = ['zupełnie bezkarnie', 'całkowicie bezczelnie', 'o poglądach na lewo od komunizmu', 'celowo i świadomie', 'z premedytacją', 'od czasów Okrągłego Stołu', 'w ramach postępu']
 const Czwarta = ['nawołują do podniesienia podatków', 'próbują wyrzucić kierowców z miast', 'próbują skłócić Polskę z Rosją ', 'głoszą brednie o globalnym ociepleniu', 'zakazują posiadania broni', 'nie dopuszczają prawicy do władzy', 'uczą dzieci homoseksualizmu']
 const Piąta = ['bo dzięki temu mogą kraść', 'bo dostają za to pieniądze', 'bo tak się uczy w państwowej szkole', 'bo bez tego (tfu!) demokracja nie moze istnieć', 'bo głupich jest więcej nit mądrych', 'bo chcą tworzyć raj na ziemi', 'bo chcą niszczyć cywilizację białego człowieka']
@@ -30,9 +30,7 @@ const zdanie = () => {
         return Math.floor(Math.random()*Szósta.length)
     }
 
-    console.log(`${Pierwsza[num1()]} ${Druga[num2()]} ${Trzecia[num3()]} ${Czwarta[num4()]} ${Piąta[num5()]} ${Szósta[num6()]}!`)
-    
-    return ''
-}
+    const zdanie = document.querySelector("h2")
+    zdanie.innerHTML = `${Pierwsza[num1()]} ${Druga[num2()]} ${Trzecia[num3()]} ${Czwarta[num4()]} ${Piąta[num5()]} ${Szósta[num6()]}!`
 
-zdanie()
+}
